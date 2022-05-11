71b6b8283b6062c3cf6dd74894a90ddf5f89d37a
Håkon Løvdal <kode@denkule.no>
2022-05-12 01:00:27 +0200

    Add copyright notice

--- git-resolve-conflict-using-kdiff3.pl
+++ git-resolve-conflict-using-kdiff3.pl
@@ -1,4 +1,21 @@
 #!/usr/bin/env perl
+
+#    git-resolve-conflict-using-kdiff3
+#    Copyright (C) 2022 Håkon Løvdal <kode@denkule.no>
+#
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU General Public License as published by
+#    the Free Software Foundation, either version 3 of the License, or
+#    (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU General Public License for more details.
+#
+#    You should have received a copy of the GNU General Public License
+#    along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
 use warnings;
 use strict;
 # use diagnostics; Depends on perl_pods package on cygwin (not installed by default, https://stackoverflow.com/a/36538115/23118). Not available in git-for-windows.
